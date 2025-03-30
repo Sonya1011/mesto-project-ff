f3d2c32afbf6b1316b4137b2ea81b14a9cde36b3
sonya <sofiv20@icloud.com>
2025-03-30 19:56:21 +0300

    первая попытка

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+https://github.com/Sonya1011/mesto-project-ff
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,29 @@
 // @todo: Темплейт карточки
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+
+function createCard (name, link, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardPlaces = cardTemplate.querySelector('.places__item').cloneNode(true); 
+  const cardImage = cardPlaces.querySelector('.card__image');
+  const cardDeleteButton = cardPlaces.querySelector('.card__delete-button');
+  const cardTitle = cardPlaces.querySelector('.card__title');
+
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+
+  cardDeleteButton.addEventListener('click', function() {
+    const deleteCard = cardDeleteButton.closest('.places__item');
+    deleteCard.remove();
+});
+
+  return cardPlaces;
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  placesList.append(createCard(initialCards[i].name, initialCards[i].link, initialCards.deleteCard))
+};
 
 // @todo: DOM узлы
 
