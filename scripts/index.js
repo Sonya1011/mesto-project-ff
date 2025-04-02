b51b2417522f4267ad9d325695be6a660386b476
sonya <sofiv20@icloud.com>
2025-04-02 22:33:19 +0300

    вторая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,37 @@
 // @todo: Темплейт карточки
-const content = document.querySelector('.content');
-const placesList = content.querySelector('.places__list');
+const content = document.querySelector('.content'); 
+const placesList = content.querySelector('.places__list'); 
+const cardTemplate = document.querySelector('#card-template').content; 
 
-function createCard (name, link, deleteCard) {
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardPlaces = cardTemplate.querySelector('.places__item').cloneNode(true); 
-  const cardImage = cardPlaces.querySelector('.card__image');
-  const cardDeleteButton = cardPlaces.querySelector('.card__delete-button');
-  const cardTitle = cardPlaces.querySelector('.card__title');
 
-  cardImage.src = link;
-  cardImage.alt = name;
-  cardTitle.textContent = name;
+function createCard (name, link, handlerdeletecard) { 
+  const cardPlaces = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardImage = cardPlaces.querySelector('.card__image'); 
+  const cardTitle = cardPlaces.querySelector('.card__title'); 
+  const cardButton = cardPlaces.querySelector('.card__delete-button');
 
-  cardDeleteButton.addEventListener('click', function() {
-    const deleteCard = cardDeleteButton.closest('.places__item');
-    deleteCard.remove();
-});
+  cardImage.src = link; 
+  cardImage.alt = name; 
+  cardTitle.textContent = name; 
 
-  return cardPlaces;
+  cardButton.addEventListener('click', () => {
+    handlerdeletecard(cardPlaces);
+  });
+
+  return cardPlaces;  
+};
+
+function deleteCard(elementCard) {
+  if(!elementCard) return;
+  elementCard.remove();
 }
 
-for (let i = 0; i < initialCards.length; i++) {
-  placesList.append(createCard(initialCards[i].name, initialCards[i].link, initialCards.deleteCard))
+
+for (let i = 0; i < initialCards.length; i++) { 
+  placesList.append(createCard(initialCards[i].name, initialCards[i].link, deleteCard)) 
 };
+
+
 
 // @todo: DOM узлы
 
